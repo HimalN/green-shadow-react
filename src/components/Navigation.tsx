aa9590affa41d0893aa891c2b7c1f5db0032acb8
HimalN <himalnimsara88@gmail.com>
2025-01-19 23:40:52 +0530

    ->UI Updates

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -23,9 +23,6 @@ export const Navigation = () => {
     return (
         <nav className="navbar navbar-expand-lg  nav" id="navbar">
             <div className="container-fluid">
-                {/*<a className="navbar-brand" href="#">*/}
-                {/*    <img id="navbar-image" src="assets/images/login-image.png"/>*/}
-                {/*</a>*/}
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
