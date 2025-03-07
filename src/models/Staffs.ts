6d746bfe8cdfaec0af3363a948927902d714dec8
HimalN <himalnimsara88@gmail.com>
2025-03-08 00:01:33 +0530

    Field

--- src/models/Staffs.ts
+++ src/models/Staffs.ts
@@ -17,22 +17,28 @@ export default class Staffs {
     field_code!:string;
 
 
-    constructor(staff_id: string, address_01: string, address_02: string, address_03: string, address_04: string, address_05: string, designation: string, dob: string, email: string, first_name: string, gender: string, joined_date: string, last_name: string, phone_no: string, role: string, field_code: string) {
-        this.staff_id = staff_id;
-        this.address_01 = address_01;
-        this.address_02 = address_02;
-        this.address_03 = address_03;
-        this.address_04 = address_04;
-        this.address_05 = address_05;
-        this.designation = designation;
-        this.dob = dob;
-        this.email = email;
-        this.first_name = first_name;
-        this.gender = gender;
-        this.joined_date = joined_date;
-        this.last_name = last_name;
-        this.phone_no = phone_no;
-        this.role = role;
-        this.field_code = field_code;
-    }
+
+
+
+
+
+    //
+    // constructor(staff_id: string, address_01: string, address_02: string, address_03: string, address_04: string, address_05: string, designation: string, dob: string, email: string, first_name: string, gender: string, joined_date: string, last_name: string, phone_no: string, role: string, field_code: string) {
+    //     this.staff_id = staff_id;
+    //     this.address_01 = address_01;
+    //     this.address_02 = address_02;
+    //     this.address_03 = address_03;
+    //     this.address_04 = address_04;
+    //     this.address_05 = address_05;
+    //     this.designation = designation;
+    //     this.dob = dob;
+    //     this.email = email;
+    //     this.first_name = first_name;
+    //     this.gender = gender;
+    //     this.joined_date = joined_date;
+    //     this.last_name = last_name;
+    //     this.phone_no = phone_no;
+    //     this.role = role;
+    //     this.field_code = field_code;
+    // }
 }
